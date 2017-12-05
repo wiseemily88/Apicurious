4b8ef554f7714d8da42b3154efbce3581d0f3f1b
Emily Wise <wise.m.emily@gmail.com>
2017-12-05 15:50:18 -0700

    updated find or create with block

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
   def self.from_omniauth(auth_info)
-    user = User.find_or_create_by(uid: auth_info[:uid]) do
+     User.find_or_create_by(uid: auth_info[:uid]) do |user|
       user.uid                = auth_info.uid
       user.login              = auth_info.extra.raw_info.login
       user.token              = auth_info.credentials.token
