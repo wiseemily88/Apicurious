d46caeada22c114c496e2466ad08d1a0bc927bc2
Emily Wise <wise.m.emily@gmail.com>
2017-12-06 20:38:14 -0700

    add check token method

--- app/controllers/githubusers_controller.rb
+++ app/controllers/githubusers_controller.rb
@@ -1,8 +1,8 @@
 class GithubusersController < ApplicationController
 
 
-
   def show
+
     conn = Faraday.new(url: "https://api.github.com") do |faraday|
       faraday.params['access_token'] = current_user.token
       faraday.adapter  Faraday.default_adapter
@@ -10,7 +10,7 @@ class GithubusersController < ApplicationController
     response = conn.get("/users/wiseemily88")
     attrs = JSON.parse(response.body, symbolize_names: true)
     @github_user = Githubuser.new(attrs)
-  
+
 
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,19 @@
 class User < ApplicationRecord
 
   def self.from_omniauth(auth_info)
-     User.find_or_create_by(uid: auth_info[:uid]) do |user|
+
+     user = User.find_or_create_by(uid: auth_info[:uid]) do |user|
       user.uid                = auth_info.uid
       user.login              = auth_info.extra.raw_info.login
       user.token              = auth_info.credentials.token
+    end
+    check_token_status(user, auth_info)
+    user
+  end
+
+  def self.check_token_status(user, auth_info)
+    if user.token != auth_info.credentials.token
+      user.update(token: auth_info.credentials.token)
     end
   end
 end
